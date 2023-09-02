4188119a9b4e60c4e2cb05837c4121c1bdc3967d
sumina729 <114402652+sumina729@users.noreply.github.com>
2023-09-02 20:54:53 +0900

    인터페이스, 클래스 생성, selectMenu() 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        WordManager wordManager = new WordManager();
+        wordManager.selectMenu();
         System.out.println("Hello world!");
     }
 }
